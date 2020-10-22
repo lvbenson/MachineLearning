f0882db0bd8fce51b03b9bbb053483e14297205f
Lauren Benson <lvbenson@iu.edu>
2020-10-22 00:21:27 -0400

    update loss function

--- Assignment3/kernelsvm.py
+++ Assignment3/kernelsvm.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib
 from sklearn.model_selection import train_test_split
 from matplotlib import pyplot as plt
-import seaborn as sn
+import math
 
 #create dataset of points
 f=open('/Users/benso/Desktop/Projects/MachineLearning/Assignment3/hw3_data2.txt',"r")
@@ -53,28 +53,54 @@ dataset = np.insert(X,2,-1,axis=1)
 #we have coordinates for our X (training) data, and a target vector
 
 def rbf_kernel(X,Y,gamma):
-
+    alpha = np.zeros(X.shape[0])
     K = np.zeros((X.shape[0],Y.shape[0]))
     for i,x in enumerate(X): 
         for j,y in enumerate(Y):
             K[i,j] = np.exp(-gamma*np.linalg.norm(x-y)**2) #actual rbf alg
     #print(K)
-    return K #this gives the actual covariance
+    #return K #this gives the actual covariance
+    epochs = 100
+    for epoch in range(epochs):
+        size = X.shape[0]
+        for i in range(size):
+            sum = 0
+            val = 0
+            for j in range(size):
+                val = alpha[j] * Y[j] * K[i,j]
+                sum = sum + val
+            if sum <= 0:
+                val = -1
+            elif sum > 0:
+                val = 1
+            if val != Y[i]:
+                alpha[i] = alpha[i] + 1
+    #check accuracy
+    print(alpha)
+    return alpha
 
 dataset = np.array(dataset)
 target = np.array(target)
 matrix = rbf_kernel(dataset,target,1.0)
 
-#how do I test the accuracy?
-
-#sn.heatmap(matrix, annot=True, fmt='g')
-#plt.show()
-
 """
-for d, sample in enumerate(dataset):
-    if target[d] == -1:
-        plt.scatter(sample[0], sample[1], s=100, marker='o', c='blue',linewidths=2)
-    else:
-        plt.scatter(sample[0], sample[1], s=100, marker='*', c='red',linewidths=2)
 
-"""
+
+def compute_efficiency(train_X,train_Y,test_X,test_Y,alpha):
+    m = test_Y.size
+    right = 0
+    for i in range(m):
+        s = 0
+        for a, x_train,y_train  in zip(alpha, train_X,train_Y):
+            s += a * y_train * rbf(test_X[i],x_train)
+        if s >0:
+            s = 1
+        elif s <=0:
+            s = -1
+        if test_Y[i] == s:
+            right +=1
+
+    print " Correct : ",right," Accuracy : ",right*100/test_X.shape[0]
+        #return y_predict
+
+    """
