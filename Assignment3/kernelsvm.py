cb6c1ccf4121715e3f5b532d22d474cc454babd7
Lauren Benson <lvbenson@iu.edu>
2020-10-22 00:29:01 -0400

    loss function update

--- Assignment3/kernelsvm.py
+++ Assignment3/kernelsvm.py
@@ -53,54 +53,33 @@ dataset = np.insert(X,2,-1,axis=1)
 #we have coordinates for our X (training) data, and a target vector
 
 def rbf_kernel(X,Y,gamma):
-    alpha = np.zeros(X.shape[0])
+    alpha = np.zeros(X.shape[0]) #actual vector to update
     K = np.zeros((X.shape[0],Y.shape[0]))
     for i,x in enumerate(X): 
         for j,y in enumerate(Y):
-            K[i,j] = np.exp(-gamma*np.linalg.norm(x-y)**2) #actual rbf alg
+            K[i,j] = np.exp(-gamma*np.linalg.norm(x-y)**2) # rbf alg
     #print(K)
-    #return K #this gives the actual covariance
+    #return K #this gives the covariance kinda
     epochs = 100
     for epoch in range(epochs):
         size = X.shape[0]
         for i in range(size):
-            sum = 0
-            val = 0
+            update = 0
+            check = 0
             for j in range(size):
-                val = alpha[j] * Y[j] * K[i,j]
-                sum = sum + val
-            if sum <= 0:
-                val = -1
-            elif sum > 0:
-                val = 1
-            if val != Y[i]:
-                alpha[i] = alpha[i] + 1
+                check = alpha[j] * Y[j] * K[i,j] #kind of like the dot product thing before
+                update = update + check
+            if update <= 0:
+                check = -1 #correctly classified
+            elif update > 0: 
+                check = 1 #correctly classified
+            if check != Y[i]: #if incorrectly classified
+                alpha[i] = alpha[i] + 1 #update alpha
     #check accuracy
     print(alpha)
     return alpha
 
 dataset = np.array(dataset)
 target = np.array(target)
-matrix = rbf_kernel(dataset,target,1.0)
+vector = rbf_kernel(dataset,target,1.0)
 
-"""
-
-
-def compute_efficiency(train_X,train_Y,test_X,test_Y,alpha):
-    m = test_Y.size
-    right = 0
-    for i in range(m):
-        s = 0
-        for a, x_train,y_train  in zip(alpha, train_X,train_Y):
-            s += a * y_train * rbf(test_X[i],x_train)
-        if s >0:
-            s = 1
-        elif s <=0:
-            s = -1
-        if test_Y[i] == s:
-            right +=1
-
-    print " Correct : ",right," Accuracy : ",right*100/test_X.shape[0]
-        #return y_predict
-
-    """
