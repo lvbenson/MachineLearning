6f42076c4d45288b4e4f6cc9e0391f6112d2c1e5
Benson <lvbenson@iu.edu>
2020-10-26 18:04:27 -0400

    accuracy

--- Assignment3/kernelsvm.py
+++ Assignment3/kernelsvm.py
@@ -52,7 +52,8 @@ f.close()
 X = np.array(data,dtype=float)
 Y = np.array(target,dtype=float)
 #create test, train vectors
-x_train,y_train,x_test,y_test = train_test_split(X,Y)
+x_train,x_test,y_train,y_test = train_test_split(X,Y)
+#print(y_train)
 
 #3600 data points
 #we have coordinates for our X (training) data, and a target vector
@@ -70,8 +71,6 @@ def CostFunc(x,w,classification):
     #loss func, non-linear svm
     #cost 0:
     class_0 = max(0, 1 - w.T.dot(x))
-    print(1 - w.T.dot(x))
-    print(class_0)
     class_1 = max(0, 1 + w.T.dot(x))
     if classification == 1:
         return class_0
@@ -89,20 +88,19 @@ def Loss_Calculation(w,K,X,Y,reg_term=1,C=1): #K is 2700 x 2700 matrix (for trai
         c_1 = CostFunc(f_i,w,1) #returns either 1 or -1
         c_0 = CostFunc(f_i,w,-1) #returns either 1 or -1
 
-        loss += y[i]*c_1 + (1-y[i])*c_0 #y is float,
+        loss += Y[i]*c_1 + (1-Y[i])*c_0 #y is float,
     
     loss = C*loss
     loss = reg_term*w.T.dot(w)
 
     #if -y[i]*w.T.dot(x[i]) < 1:
      #   L = [i for i in range(len(y))]
-    
-    L = [i for i in range(len(y)) if -y[i]*classification(i,w,y,K)<1]
+    L = [i for i in range(len(Y)) if -Y[i]*classification(i,w,Y,K)<1]
     
     #we need to calculate the change in w, or how to wiggle our w vector
     #according to each classification
     dw = np.array([
-        -y[i] * sum([y[j]*K[j,i] for j in L]) for i in range(len(y))
+        -Y[i] * sum([Y[j]*K[j,i] for j in L]) for i in range(len(Y))
     ])
 
 
@@ -110,16 +108,17 @@ def Loss_Calculation(w,K,X,Y,reg_term=1,C=1): #K is 2700 x 2700 matrix (for trai
 
 
 def classification(index_x,w,Y,K):
-    #w is len(X)
+    #w has been calculated. So has k. Now, we need to figure out the classification of every point in our data.
+    #for every element in Y (our original classifications), we multiply by the corresponding Kernel value, and the corresponding
+    #weight vector value. We do this for everything in our dataset, X, and store it in a list. 
 
     fx = 0
     for i in range(len(Y)):
         fx += Y[i]*K[index_x][i]*w[i]
     return fx
 
-def SVM(X,Y,K,epochs=1,learn_rate=1,reg_term=1,C=1,Gamma=1):
-    #initialize random weights
-    #w = np.zeros_like(X[0])
+def SVM(X,Y,K,epochs=5,learn_rate=1,reg_term=1,C=1,Gamma=1):
+    #initialize w vector. This is the size of X
     w = np.zeros(X.shape[0])
     for epoch in range(epochs):
         loss_list = []
@@ -128,18 +127,18 @@ def SVM(X,Y,K,epochs=1,learn_rate=1,reg_term=1,C=1,Gamma=1):
         loss_list.append(loss)
 
         w = np.subtract(w,dw)
-    
+    print(w.shape)
     return w
 
     
-def pipeline(X,Y,epochs=1,learn_rate=1,reg_term=1,C=1,Gamma=1):
+def pipeline(X,Y,epochs=5,learn_rate=1,reg_term=1,C=1,Gamma=1):
     K = rbf_kernel(X,Gamma)
 
     w_vector = SVM(X,Y,K,epochs,learn_rate)
 
     #need to classify everything in X 
     #things to classify:
-    classification = [classification(i,w_vector,Y,K) for i in range(len(x))]
+    classifications = [classification(i,w_vector,Y,K) for i in range(len(X))]
     """
     size = range(len(X))
 
@@ -147,10 +146,51 @@ def pipeline(X,Y,epochs=1,learn_rate=1,reg_term=1,C=1,Gamma=1):
     for index_x in size:
         classify_list.append(classification(index_x,w_vector,Y,K,))
     """
-    return classification,w_vector
+    return classifications,w_vector
 
 
-overall_shits = pipeline(x_train,y_train)
-print(np.sign(overall_shits))
+classes,w = pipeline(x_train,y_train)
+correct_classify = []
+incorrect_classify = []
+
+#check out the training data accuracy
+
+for i,y in zip(classes,y_train):
+    if i > 0 and y == 1:
+        correct_classify.append(1)
+    elif i < 0 and y == -1:
+        correct_classify.append(1)
+    else:
+        incorrect_classify.append(1)
+total_correct = (len(correct_classify)) / (len(correct_classify) + len(incorrect_classify))
+print(total_correct,' :accuracy of training data')
 
 
+#check out the test data accuracy. only calculate kernel and classes, use the w that is already calculated
+K = rbf_kernel(x_test,1)
+classifications_test = [classification(i,w,y_test,K) for i in range(len(x_test))]
+
+correct_classify_test = []
+incorrect_classify_test = []
+
+#check out the training data accuracy
+
+for i,y in zip(classifications_test,y_test):
+    if i > 0 and y == 1:
+        correct_classify_test.append(1)
+    elif i < 0 and y == -1:
+        correct_classify_test.append(1)
+    else:
+        incorrect_classify_test.append(1)
+total_correct_test = (len(correct_classify_test)) / (len(correct_classify_test) + len(incorrect_classify_test))
+print(total_correct_test,' :accuracy of test data')
+
+for d, sample in enumerate(x_test):
+    if y_test[d] == -1:
+        plt.scatter(sample[0], sample[1], s=100, marker='o', c='blue',linewidths=2)
+    else:
+        plt.scatter(sample[0], sample[1], s=100, marker='*', c='green',linewidths=2)
+
+plt.plot(w,c='red',label='hyperplane')
+plt.legend()
+plt.show()
